e11d5aad1f2fd86506f0ce41ee66f80d796fd26b
alzemand <edilson_alz@hotmail.com>
2016-08-01 16:14:10 -0300

    serve

---
+++ controllers/default.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a sample controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+#########################################################################
+
+def index():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simply replace the two lines below with:
+    return auth.wiki()
+    """
+    response.flash = T("Hello World")
+    return dict(message=T('Welcome to web2py!'))
+
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/manage_users (requires membership in
+    http://..../[app]/default/user/bulk_register
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+
+# http://localhost:8000/sigedo/default/aluno
+
+def aluno():
+    form = SQLFORM(Aluno)
+    if form.process().accepted:
+        session.flash = 'Novo Aluno cadastrado: %s' % form.vars.titulo
+        redirect(URL('aluno'))
+    elif form.errors:
+        response.flash = 'Erros no formulário!'
+    else:
+        if not response.flash:
+            response.flash = 'Preencha o formulário!'
+    return dict(form=form)
+    
+# http://localhost:8000/sigedo/default/empresa
+
+def empresa():
+    form = SQLFORM(Empresa)
+    if form.process().accepted:
+        session.flash = 'Nova empresa cadastrada: %s' % form.vars.titulo
+        redirect(URL('aluno'))
+    elif form.errors:
+        response.flash = 'Erros no formulário!'
+    else:
+        if not response.flash:
+            response.flash = 'Preencha o formulário!'
+    return dict(form=form)
+
+# http://localhost:8000/sigedo/default/estagio
+
+def estagio():
+    form = SQLFORM(Estagio)
+    if form.process().accepted:
+        session.flash = 'Novo estagio cadastrado: %s' % form.vars.titulo
+        redirect(URL('aluno'))
+    elif form.errors:
+        response.flash = 'Erros no formulário!'
+    else:
+        if not response.flash:
+            response.flash = 'Preencha o formulário!'
+    return dict(form=form)
