0c56de7aec51e58048db564ac396efe3f46e9be5
alzemand <edilson_alz@hotmail.com>
2016-08-01 13:31:34 -0300

    add MySQLdb connection

---
+++ db_connect_test.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+# author: Edilson Alzemand, at SIGEDO
+# data base connect
+# apt-get install python-mysqldb
+
+import MySQLdb
+
+# Conectar ao banco de dados
+
+con = MySQLdb.connect ( host = "localhost",
+                        user = "root",
+                        passwd = "*****",
+                        db = "sigedo")
+                        
+# Ler dados do banco de dados
+cursor = con.cursor ()
+cursor.execute ("SELECT VERSION()")
+res = cursor.fetchone ()
+print "server version:", res[0]
+
+# Gravar\alterar dados no bando de dados
+
+cursor.execute ("INSERT INTO sigedo.aluno (al_matricula, al_nome, al_login, al_email) VALUES ('1401130027', 'Edilson Alzemand', 1401130027, 'edilson.alzemand@gmail.com')")
+
+con.commit()
+
+cursor.close ()
+con.close ()
